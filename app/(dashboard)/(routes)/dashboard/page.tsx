d3036fad8e4c45ed1f32d38085394b0733249680
Srujan Dharma <Srujan.23bcs10107@ms.sst.scaler.com>
2024-05-16 23:49:05 +0530

    changes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -51,7 +51,7 @@ const Landing = () => {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Delve into the capabilities of AI
+          Dive into the capabilities of AI
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
           Chat with smartest AI - Experience the power Of AI
