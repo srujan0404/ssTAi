c4aa93170b79e0e435f7380bcfbccbd4b7b834dc
Srujan Dharma <Srujan.23bcs10107@ms.sst.scaler.com>
2024-05-22 22:07:29 +0530

    cahnges in dashboard

--- components/loader.tsx
+++ components/loader.tsx
@@ -11,7 +11,7 @@ export const Loader = () => {
                 />
             </div>
             <p className="text-sm text-muted-foreground"> 
-                ssTAi is Waiting please Wait!
+                ssTAi is Thinking please Wait!
             </p>
         </div>
     )
